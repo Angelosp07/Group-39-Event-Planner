4d37b33bc22f7f9ccd02a6e41d52ddb78f5aed39
Angelos Pappas <ap1315@exeter.ac.uk>
2026-01-21 16:16:26 +0000

    small touch up

---
+++ event_planner.py
@@ -0,0 +1,7 @@
+""" Module containing brute force and dynamic programming solutions to event planner problem """
+
+from helper import parse_file
+
+total_activities, total_time, total_money, activities = parse_file("input_files/input_10.txt")
+
+print(total_activities, total_time, total_money, activities)
